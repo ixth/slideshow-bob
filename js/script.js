1ad6b2d7bb6374fa072b6b27ddd5bc2661313316
Mikhail Menshikov <ainop@ya.ru>
2012-06-23 15:39:01 +0400

    Initial version

---
+++ js/script.js
@@ -0,0 +1,55 @@
+(function (global) {
+	var slidesApp = new SlidesApp();
+
+	slidesApp.init({
+		'frame': $('.slides').get(0),
+		'toolbar': $('.toolbar_nav .toolbar__form').get(0)
+	});
+
+	// Not sure if I should place DOM binding code here or in some module method.
+	$(window).on('resize', function (e) {
+		slidesApp.scale();
+	});
+
+	$(window).on('keydown', function (e) {
+		if (e.keyCode === 37) {
+			slidesApp.prevSlide();
+		} else if (e.keyCode === 39) {
+			slidesApp.nextSlide();
+		} else if (e.keyCode === 36) {
+			slidesApp.setSlide(0);
+		} else if (e.keyCode === 35) {
+			slidesApp.setSlide(-1);
+		}
+	});
+
+	$(window).on('click', '.button', function (e) {
+		var button = $(e.target);
+		if (button.hasClass('button_prev')) {
+			slidesApp.prevSlide();
+		} else if (button.hasClass('button_next')) {
+			slidesApp.nextSlide();
+		} else if (button.hasClass('button_first')) {
+			slidesApp.setSlide(0);
+		} else if (button.hasClass('button_last')) {
+			slidesApp.setSlide(-1);
+		}
+	});
+
+	slidesApp._toolbar.elements.slide_current.addEventListener('keydown', function (e) {
+		e.stopPropagation();
+	});
+
+	slidesApp._toolbar.elements.slide_current.addEventListener('keypress', function (e) {
+		if (/\D/.test(String.fromCharCode(e.keyCode))) {
+			e.preventDefault();
+		}
+	});
+
+	slidesApp._toolbar.elements.slide_current.addEventListener('change', function (e) {
+		var slide = parseInt(e.target.value, 10);
+		slidesApp.setSlide(slide - 1);
+	});
+
+	global.slidesApp = slidesApp;
+})(this);
---
+++ js/slides-app.js
@@ -0,0 +1,65 @@
+var SlidesApp = function () {
+};
+
+SlidesApp.prototype.init = function (options) {
+	this._toolbar = options.toolbar;
+	this._frame = options.frame;
+	this._slides = $(this._frame).find('.slides__item');
+
+	this.setSlide(0);
+	this.updateToolbar();
+	this.scale();
+
+};
+
+SlidesApp.prototype.scale = function () {
+	var body = this._frame;
+	var scale = Math.min(
+		window.innerWidth / body.clientWidth,
+		(window.innerHeight - this._toolbar.parentNode.offsetHeight) / body.clientHeight
+	);
+	var value = 'scale('+scale+')';
+
+	// TODO: use PrefixFree or StyleFix?
+	$(body).css({
+		'transform': value,
+		'-o-transform': value,
+		'-ms-transform': value,
+		'-moz-transform': value,
+		'-webkit-transform': value
+	})
+};
+
+SlidesApp.prototype.updateToolbar = function () {
+	if (!this._toolbar.elements.slides_total.value) {
+		this._toolbar.elements.slides_total.value = this._slides.length;
+	}
+	this._toolbar.elements.slide_current.value = this._current + 1;
+
+	this._toolbar.elements.button_first.disabled =
+	this._toolbar.elements.button_prev.disabled = (this._current === 0);
+	this._toolbar.elements.button_last.disabled =
+	this._toolbar.elements.button_next.disabled = (this._current + 1 === this._slides.length);
+};
+
+SlidesApp.prototype.setSlide = function (i) {
+	if (i < 0) {
+		i = this._slides.length + i;
+	}
+
+	if (i >= this._slides.length) return;
+
+	this._current = i;
+	$('.slides__item_active').removeClass('slides__item_active');
+	$('.slides__item').eq(this._current).addClass('slides__item_active');
+
+	this.updateToolbar();
+};
+
+SlidesApp.prototype.prevSlide = function () {
+	this.setSlide(this._current - 1);
+};
+
+SlidesApp.prototype.nextSlide = function () {
+	this.setSlide(this._current + 1);
+};
